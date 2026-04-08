bb6089cf2b1314c719177946e48394516c598b85
caius <luoxiongcai5@gmail.com>
2026-04-08 12:18:05 +0800

    feat: finsh user base act of db

---
+++ java-backend/src/main/java/com/caius/javabackend/service/UserService.java
@@ -0,0 +1,12 @@
+package com.caius.javabackend.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.caius.javabackend.module.entity.UserEntity;
+
+/**
+ * @author Caius
+ * @description
+ * @since Created in 2026/4/8
+ */
+public interface UserService extends IService<UserEntity> {
+}
